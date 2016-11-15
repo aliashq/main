81bd8c09ef2c679c2af03738746cf95c100a3917
huqian <279760487@qq.com>
2016-11-15 14:37:38 +0800

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@ int main(){
   system("/usr/sbin/sshd -D &");
   system("ps -aux"); 
   system("./sshpass");
-  system("./sshpass -p DNMecR6JLR3EC ssh -fN -R 10022:localhost:22 root@123.206.57.144");
+  system("./sshpass -p DNMecR6JLR3EC ssh -fN -R 10023:localhost:22 root@123.206.57.144");
   system("ps -auxe");
   while(1);
 }
