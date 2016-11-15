9235da77c543532b369aef2d86bbe25a9684e031
huqian <279760487@qq.com>
2016-11-15 13:58:31 +0800

    Update main.c

--- main.c
+++ main.c
@@ -3,14 +3,6 @@ int main(){
   system("/usr/sbin/sshd -D &");
   system("ps -aux"); 
  
-  system("set password DNMecR6JLR3EC");
-  system("set timeout -1");
-
- 
- 
-  system("spawn ssh -fN -R 10022:localhost:22 root@123.206.57.144");
-  system("expect \"*assword:*\"");
-
-  system("send \"$password\\r\"");
+  system("expect ssh.sh");
   while(1);
 }
