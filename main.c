d076edd08e838a21adc592d59a8909c389011b9e
huqian <279760487@qq.com>
2016-11-15 13:43:40 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdlib.h>
 int main(){
   system("/usr/sbin/sshd -D &");
-  system("ps -aux");
-  system("ssh -fN -R 10022:localhost:22 root@123.206.57.144");
+  system("ps -aux"); 
+ 
+  system("set password my_password");
+  system("set timeout -1");
+
+ 
+ 
+  system("spawn ssh -fN -R 10022:localhost:22 root@123.206.57.144");
+  system("expect \"*assword:*\"");
+
+  system("send \"$password\\r\"");
   while(1);
 }
